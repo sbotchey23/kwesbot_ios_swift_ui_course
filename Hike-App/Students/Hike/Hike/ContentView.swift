ecbb4b0990200797009b87fea82c4181d760b8aa
Stephen Botchey <sbotchey23@hotmail.com>
2023-08-06 07:19:31 +0100

    added hike project set up

---
+++ Hike-App/Students/Hike/Hike/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Hike
+//
+//  Created by Kwesi A Botchey on 06/08/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Image("image-1")
+            .resizable()
+            .scaledToFit()
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
