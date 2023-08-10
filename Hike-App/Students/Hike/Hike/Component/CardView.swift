9253f2ce7b67e7e7c35942760089fcf4e31d8f9d
sbotchey23 <sbotchey23@hotmail.com>
2023-08-10 09:32:27 +0100

    updated card view

--- Hike-App/Students/Hike/Hike/Component/CardView.swift
+++ Hike-App/Students/Hike/Hike/Component/CardView.swift
@@ -13,20 +13,26 @@ struct CardView: View {
         
         ZStack {
             CustomBackgroundView()
-            ZStack {
-                Circle()
-                    .fill(
-                        LinearGradient(
-                            colors:[
-                                Color("ColorIndigoMedium"),
-                                Color("ColorSalmonLight")
-                                ],
-                                startPoint: .topLeading,
-                                endPoint: .bottomTrailing))
-                    .frame(width: 256, height: 256)
-                Image("image-1")
-                    .resizable()
-                    .scaledToFit()
+            VStack {
+                // MARK: - HEADER
+                
+                // MARK: - MAIN CONTENT
+                
+                ZStack {
+                    Circle()
+                        .fill(
+                            LinearGradient(
+                                colors:[
+                                    Color("ColorIndigoMedium"),
+                                    Color("ColorSalmonLight")
+                                    ],
+                                    startPoint: .topLeading,
+                                    endPoint: .bottomTrailing))
+                        .frame(width: 256, height: 256)
+                    Image("image-1")
+                        .resizable()
+                        .scaledToFit()
+                }
             }
         } //: CARD
         .frame(width: 320, height: 570)
