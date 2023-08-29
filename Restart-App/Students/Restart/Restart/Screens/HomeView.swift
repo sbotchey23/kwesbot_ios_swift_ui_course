13194fdcb15368c43b77677a0b001307484ccdec
sbotchey23 <sbotchey23@hotmail.com>
2023-08-29 03:35:15 +0100

    Added reusable component: CircleGroupView

--- Restart-App/Students/Restart/Restart/Screens/HomeView.swift
+++ Restart-App/Students/Restart/Restart/Screens/HomeView.swift
@@ -13,17 +13,44 @@ struct HomeView: View {
     
     // MARK: - BODY
     
-    
     var body: some View {
         VStack(spacing: 20) {
-            Text("Home view")
-                .font(.largeTitle)
+            // MARK: - HEADER
+            Spacer()
+            
+            ZStack {
+                CircleGroupView(ShapeColor: .cyan, ShapeOpacity: 0.1)
+                
+                Image("character-2")
+                    .resizable()
+                    .scaledToFit()
+                .padding()
+            }
+            
+            // MARK: - CENTER
+            Text("The time that leades to mastery depends on the intensity of our focus.")
+                .font(.title3)
+                .fontWeight(.light)
+                .foregroundColor(.secondary)
+                .multilineTextAlignment(.center)
+                .padding()
+            
+            // MARK: - FOOTER
+            Spacer()
             
             Button(action:{
                 isOnboardingViewActive = true
             }) {
+                Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
+                    .imageScale(.large)
+                
                 Text("Restart")
-            }
+                    .font(.system(.title3, design: .rounded))
+                    .fontWeight(.bold)
+            }//: BUTTON
+            .buttonStyle(.borderedProminent)
+            .buttonBorderShape(.capsule)
+            .controlSize(.large)
         } //: VSTACK
     }
 }
