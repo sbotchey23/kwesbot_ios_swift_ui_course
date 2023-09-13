d0835fead0e185980650fbb275d918f717f219fc
sbotchey23 <sbotchey23@hotmail.com>
2023-09-13 19:26:32 +0100

    started pinch project

---
+++ Pinch App/Students/Pinch/Pinch/Screen/ContentView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  Pinch
+//
+//  Created by Stephen Kwesi Botchey on 13/09/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    // MARK: - PROPERTY
+    
+    // MARK: - FUNCTION
+    
+    // MARK: - CONTENT
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+// MARK: - PREVIEW
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .preferredColorScheme(.dark)
+            .previewDevice("iPhone 13")
+    }
+}
