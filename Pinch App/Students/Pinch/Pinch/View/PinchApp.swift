b3f74dd8710fbb4084b87f5ef268112d292278e2
sbotchey23 <sbotchey23@hotmail.com>
2023-10-18 05:30:51 +0100

    re-adding Pinch project folder from commit-b59451e

---
+++ Pinch App/Students/Pinch/Pinch/View/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Stephen Kwesi Botchey on 13/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
