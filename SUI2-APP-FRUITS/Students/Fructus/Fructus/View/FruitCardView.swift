e9c94ee55dc71fde66c49dc59846ae62c32c050e
sbotchey23 <sbotchey23@hotmail.com>
2023-11-01 07:03:51 +0000

    added custom start button and card view

---
+++ SUI2-APP-FRUITS/Students/Fructus/Fructus/View/FruitCardView.swift
@@ -0,0 +1,63 @@
+//
+//  FruitCardView.swift
+//  Fructus
+//
+//  Created by Kwesi A Botchey on 01/11/2023.
+//
+
+import SwiftUI
+
+struct FruitCardView: View {
+    // MARK: - PROPERTIES
+    
+    @State private var isAnimating: Bool = false
+    
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        ZStack {
+            VStack(spacing: 20) {
+                // FRUIT: IMAGE
+                Image("blueberry")
+                    .resizable()
+                    .scaledToFit()
+                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.25), radius: 8, x: 6, y: 8)
+                // FRUIT: TITLE
+                
+                Text("Blueberry")
+                    .foregroundColor(Color.white)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.25), radius: 2, x: 2, y: 2)
+                
+                // FRUIT: HEADLINE
+                Text("Blueberries are a sweet, nutritious and wildy popular fruit all over the world.")
+                    .foregroundColor(Color.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 16)
+                    .frame(maxWidth: 480)
+                
+                // BUTTON: START
+                StartButtonView()
+            } //: VSTACK
+        } //: ZSTACK
+        .onAppear{
+            withAnimation(.easeOut(duration: 0.5)) {
+                isAnimating = true
+            }
+        }
+        .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
+        .background(LinearGradient(gradient: Gradient(colors: [Color("ColorBlueberryLight"), Color("ColorBlueberryDark")]), startPoint: .top, endPoint: .bottom))
+        .cornerRadius(20)
+        .ignoresSafeArea() // this is my addition for newer iPhones
+        
+    }
+}
+
+    // MARK: - PREVIEW
+
+#Preview {
+    FruitCardView()
+        .previewLayout(.fixed(width: 320, height: 640))
+}
