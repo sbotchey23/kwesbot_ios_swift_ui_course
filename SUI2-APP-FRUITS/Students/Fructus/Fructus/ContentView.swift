c090562a8356a85566d035f3604e00464f32757b
sbotchey23 <sbotchey23@hotmail.com>
2023-11-01 03:58:03 +0000

    added new fructus project files

---
+++ SUI2-APP-FRUITS/Students/Fructus/Fructus/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fructus
+//
+//  Created by Kwesi A Botchey on 27/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SUI2-APP-FRUITS/Students/Fructus/Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Kwesi A Botchey on 27/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
