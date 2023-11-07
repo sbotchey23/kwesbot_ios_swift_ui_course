042f12c0b7ffd431290df0868cec9c60c65a918a
sbotchey23 <sbotchey23@hotmail.com>
2023-11-07 03:55:13 +0000

    updated fruitRowView

--- SUI2-APP-FRUITS/Students/Fructus/Fructus/View/FruitRowView.swift
+++ SUI2-APP-FRUITS/Students/Fructus/Fructus/View/FruitRowView.swift
@@ -15,7 +15,27 @@ struct FruitRowView: View {
     
     var body: some View {
         HStack {
+            // MARK: Image
+            Image(fruit.image)
+                .renderingMode(.original)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 80, height: 80, alignment: .center)
+                .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.3), radius: 3, x: 2, y: 2)
+                .background(
+                    LinearGradient(gradient: Gradient(colors: fruit.gradientColors), startPoint: .top, endPoint: .bottom)
+                    )
+                .cornerRadius(8)
+            //: Image
             
+            VStack(alignment: .leading, spacing: 5) {
+                Text(fruit.title)
+                    .font(.title2)
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                Text(fruit.headline)
+                    .font(.caption)
+                    .foregroundColor(Color.secondary)
+            }
         } //: HSTACK
     }
 }
@@ -25,6 +45,6 @@ struct FruitRowView: View {
 #Preview {
     FruitRowView(fruit: fruitsData[0])
         .previewLayout(.sizeThatFits)
-        .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
+//        .preferredColorScheme(.dark)
         .padding()
 }
