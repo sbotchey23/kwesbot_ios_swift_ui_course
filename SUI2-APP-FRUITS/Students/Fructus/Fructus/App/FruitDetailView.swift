e5a12a12feb2fd6572a7c83819ad20a6b750eb90
sbotchey23 <sbotchey23@hotmail.com>
2023-11-08 06:47:54 +0000

    added new views and updated content view
    FruitHeaderView
    SourceLinkView
    FruitDetailView

---
+++ SUI2-APP-FRUITS/Students/Fructus/Fructus/App/FruitDetailView.swift
@@ -0,0 +1,63 @@
+//
+//  FruitDetailView.swift
+//  Fructus
+//
+//  Created by Kwesi A Botchey on 07/11/2023.
+//
+
+import SwiftUI
+
+struct FruitDetailView: View {
+    // MARK: - PROPERTIES
+    var fruit: Fruit
+    
+    
+    // MARK: - BODY
+    var body: some View {
+        NavigationView{
+            ScrollView(.vertical, showsIndicators: false) {
+                VStack(alignment: .center, spacing: 20) {
+                    // HEADER
+                    FruitHeaderView(fruit: fruit)
+                    
+                    VStack(alignment: .leading, spacing: 20) {
+                        // TITLE
+                        Text(fruit.title)
+                            .font(.largeTitle)
+                            .fontWeight(.heavy)
+                            .foregroundColor(fruit.gradientColors[1])
+                        
+                        // HEADLINE
+                        Text(fruit.headline)
+                            .font(.headline)
+                            .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
+                        
+                        // NUTRIENTS
+                        
+                        // SUBHEADLINE
+                        Text("Learn more about \(fruit.title)".uppercased())
+                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                            .foregroundColor(fruit.gradientColors[1])
+                        
+                        // DESCRIPTION
+                        Text(fruit.description)
+                            .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
+                        
+                        // LINK
+                        
+                    } //: VSTACK
+                    .padding(.horizontal, 20)
+                    .frame(maxWidth: 640, alignment: .center)
+                } //: VSTACK
+                .navigationBarTitle(fruit.title, displayMode: .inline)
+                .navigationBarHidden(true)
+            } //: SCROLLVIEW
+            .edgesIgnoringSafeArea(.top)
+        } //: NAVIGATIONVIEW
+    }
+}
+
+// MARK: - PREVIEW
+#Preview {
+    FruitDetailView(fruit: fruitsData[0])
+}
---
+++ SUI2-APP-FRUITS/Students/Fructus/Fructus/View/SourceLinkView.swift
@@ -0,0 +1,18 @@
+//
+//  SourceLinkView.swift
+//  Fructus
+//
+//  Created by Kwesi A Botchey on 08/11/2023.
+//
+
+import SwiftUI
+
+struct SourceLinkView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SourceLinkView()
+}
