d89d3e897726be0286d69e4406957addf3727ec7
sbotchey23 <sbotchey23@hotmail.com>
2023-11-15 06:38:51 +0000

    added africa project to student folder

---
+++ SWIFTUI-APP-AFRICA-UPDATED/Students/Africa/Africa/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Africa
+//
+//  Created by Kwesi A Botchey on 15/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
