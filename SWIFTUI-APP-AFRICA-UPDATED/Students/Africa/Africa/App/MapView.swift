e6c93dd610ff6be622ede816f96887f4b2e3c067
sbotchey23 <sbotchey23@hotmail.com>
2023-12-15 08:18:57 +0000

    Updated MapView

--- SWIFTUI-APP-AFRICA-UPDATED/Students/Africa/Africa/App/MapView.swift
+++ SWIFTUI-APP-AFRICA-UPDATED/Students/Africa/Africa/App/MapView.swift
@@ -9,10 +9,31 @@ import SwiftUI
 import MapKit
 
 struct MapView: View {
+    // MARK: - PROPERTIES
+    
+    @State private var region: MKCoordinateRegion = {
+    var mapCoordinates = CLLocationCoordinate2D(latitude: 6.600286, longitude: 16.4377599)
+    var mapZoomLevel = MKCoordinateSpan(latitudeDelta: 70.0, longitudeDelta: 70.0)
+    var mapRegion = MKCoordinateRegion(center: mapCoordinates, span: mapZoomLevel)
+
+    return mapRegion
+    }()
+    
+    let locations: [NationalParkLocation] = Bundle.main.decode("locations.json")
+        
+    // MARK: - BODY
     var body: some View {
-        Text("Map!")
+        // MARK: - No.1 BASIC MAP
+        //Map(coordinateRegion: $region)
+        
+        // MARK: - No.2 ADVANCED MAP
+        Map(coordinateRegion: $region, annotationItems: locations, annotationContent: { items in
+            
+        })
     }
 }
+
+// MARK: - PREVIEW
 
 #Preview {
     MapView()
