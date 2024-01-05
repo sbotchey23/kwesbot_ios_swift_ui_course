82aa16200c452015ed4a987842dff5e495fed4ba
sbotchey23 <sbotchey23@hotmail.com>
2024-01-05 08:42:12 +0000

    added MapAnnotationView and MotionAnimationView

--- SWIFTUI-APP-AFRICA-UPDATED/Students/Africa/Africa/App/MapView.swift
+++ SWIFTUI-APP-AFRICA-UPDATED/Students/Africa/Africa/App/MapView.swift
@@ -22,14 +22,77 @@ struct MapView: View {
     let locations: [NationalParkLocation] = Bundle.main.decode("locations.json")
         
     // MARK: - BODY
+    
     var body: some View {
         // MARK: - No.1 BASIC MAP
         //Map(coordinateRegion: $region)
         
         // MARK: - No.2 ADVANCED MAP
-        Map(coordinateRegion: $region, annotationItems: locations, annotationContent: { items in
+        Map(coordinateRegion: $region, annotationItems: locations, annotationContent: { item in
+            // (A) PIN: OLD STYLE (always static)
+            // MapPin(coordinate: item.location, tint: .accentColor)
             
-        })
+            // (B) MARKER: NEW STYLEb (always static)
+            // MapMarker(coordinate: item.location, tint: .accentColor)
+            
+            // (C) CUSTOM BASIC ANNOTATION (it could be interactive)
+            //  MapAnnotation(coordinate: item.location) {
+            // Image("logo")
+            //      .resizable()
+            //      .scaledToFit()
+            //     .frame(width: 32, height: 32, alignment: .center)
+            // } //: ANNOTATION
+            
+            // (D) CUSTOM ADVANCED ANNOTATION (it could be interactive)
+            MapAnnotation(coordinate: item.location) {
+                MapAnnotationView(location: item)
+            }
+        }) //: MAP
+        .overlay(
+            HStack(alignment: .center, spacing: 12) {
+                Image("compass")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 48, height: 48, alignment: .center)
+                
+                VStack(alignment: .leading, spacing: 3){
+                    HStack{
+                        Text("Latitude:")
+                            .font(.footnote)
+                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                            .foregroundColor(.accentColor)
+                        Spacer()
+                        Text("\(region.center.latitude)")
+                            .font(.footnote)
+                            .foregroundColor(.white)
+                    } //: HSTACK
+                    
+                    Divider()
+                    
+                    HStack{
+                        Text("Longitude:")
+                            .font(.footnote)
+                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                            .foregroundColor(.accentColor)
+                        
+                        Spacer()
+                        
+                        Text("\(region.center.longitude)")
+                            .font(.footnote)
+                            .foregroundColor(.white)
+                    } //: HSTACK
+                } //: VSTACK
+            } //: HSTACK
+                .padding(.vertical, 12)
+                .padding(.horizontal, 16)
+                .background(
+                    Color.black
+                        .cornerRadius(8)
+                        .opacity(0.6)
+                )
+                .padding()
+            , alignment: .top
+        )
     }
 }
 
---
+++ SWIFTUI-APP-AFRICA-UPDATED/Students/Africa/Africa/View/MotionAnimationView.swift
@@ -0,0 +1,24 @@
+//
+//  MotionAnimationView.swift
+//  Africa
+//
+//  Created by Kwesi A Botchey on 05/01/2024.
+//
+
+import SwiftUI
+
+struct MotionAnimationView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: - PREVIEW
+
+#Preview {
+    MotionAnimationView()
+}
