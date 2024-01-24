73e932c194146fd8c7bd4c5804bb779aff21da89
sbotchey23 <sbotchey23@hotmail.com>
2024-01-24 07:13:38 +0000

    added AnimalGridItemView, updated ContentView with simple grid

--- SWIFTUI-APP-AFRICA-UPDATED/Students/Africa/Africa/App/ContentView.swift
+++ SWIFTUI-APP-AFRICA-UPDATED/Students/Africa/Africa/App/ContentView.swift
@@ -10,22 +10,70 @@ import SwiftUI
 struct ContentView: View {
     // MARK: - PROPERTIES
     let animals: [Animal] = Bundle.main.decode("animals.json")
+    let haptics = UIImpactFeedbackGenerator(style: .medium)
+    
+    @State private var isGridViewActive: Bool = false
+
+    let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 2)
     
     // MARK: - BODY
     var body: some View {
         NavigationStack {
-            List {
-                CoverImageView()
-                    .frame(height: 300)
-                    .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
-                
-                ForEach(animals) {animal in
-                    NavigationLink(destination: AnimalDetailView(animal: animal)) {
-                        AnimalListItemView(animal: animal)
-                    } //: NAVIGATIONLINK
-                } //: LOOP
-            } //: LIST
+            Group {
+                if !isGridViewActive {
+                    List {
+                        CoverImageView()
+                            .frame(height: 300)
+                            .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                        
+                        ForEach(animals) {animal in
+                            NavigationLink(destination: AnimalDetailView(animal: animal)) {
+                                AnimalListItemView(animal: animal)
+                            } //: NAVIGATIONLINK
+                        } //: LOOP
+                    } //: LIST
+                } else {
+                    ScrollView(.vertical, showsIndicators: false) {
+                        LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+                            ForEach(animals) { animal in
+                                NavigationLink(destination: AnimalDetailView(animal: animal)) {
+                                    AnimalGridItemView(animal: animal)
+                                } //: NAVIGATIONLINK
+                            } //: LOOP
+                        } //: GRID
+                        .padding(10)
+                        .animation(/*@START_MENU_TOKEN@*/.easeIn/*@END_MENU_TOKEN@*/)
+                    } //: SCROLL
+                } //: CONDITION
+            } //: GROUP
             .navigationBarTitle("Africa", displayMode: .large)
+            .toolbar  {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    HStack(spacing: 16) {
+                        // LIST
+                        Button(action: {
+                            print("List view is activated")
+                            isGridViewActive = false
+                            haptics.impactOccurred()
+                        }) {
+                            Image(systemName: "square.fill.text.grid.1x2")
+                                .font(.title2)
+                                .foregroundColor(isGridViewActive ? .primary : .accentColor)
+                        }
+                        
+                        // GRID
+                        Button(action: {
+                            print("Grid view is activated")
+                            isGridViewActive = true
+                            haptics.impactOccurred()
+                        }) {
+                            Image(systemName: "square.grid.2x2")
+                                .font(.title2)
+                                .foregroundColor(isGridViewActive ? .accentColor : .primary)
+                        }
+                    } //: HSTACK
+                } //:BUTTONS
+            } //: TOOLBAR
         } //: NAVIGATIONSTACK
         .ignoresSafeArea(.all)
     }
