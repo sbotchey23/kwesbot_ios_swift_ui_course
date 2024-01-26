2676c945f82f162da30f330100f6732b6bd72613
sbotchey23 <sbotchey23@hotmail.com>
2024-01-26 06:30:27 +0000

    updated GridLayoutView, ContentView, added CreditsView
    Added stickers assets from the Africa app to the iMessage
    Added CreditsView

---
+++ SWIFTUI-APP-AFRICA-UPDATED/Students/Africa/Africa/View/CreditsView.swift
@@ -0,0 +1,23 @@
+//
+//  CreditsView.swift
+//  Africa
+//
+//  Created by Kwesi A Botchey on 26/01/2024.
+//
+
+import SwiftUI
+
+struct CreditsView: View {
+    var body: some View {
+        Text("""
+    Copyright © Kwesi A. Botchey
+    All rights reserved
+    Better Apps ♡ Less Code
+    """)
+    }
+}
+
+#Preview(traits: .sizeThatFitsLayout) {
+    CreditsView()
+        .padding()
+}
