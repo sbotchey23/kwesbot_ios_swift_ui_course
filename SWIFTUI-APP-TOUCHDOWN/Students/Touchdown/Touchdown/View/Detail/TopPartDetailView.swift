636c96601dc39d1a1efc557bba312060f7e779c2
sbotchey23 <sbotchey23@hotmail.com>
2024-02-11 07:25:02 +0000

    removed: TopPartDetailView

--- SWIFTUI-APP-TOUCHDOWN/Students/Touchdown/Touchdown/View/Detail/TopPartDetailView.swift
+++ SWIFTUI-APP-TOUCHDOWN/Students/Touchdown/Touchdown/View/Detail/TopPartDetailView.swift
@@ -8,43 +8,11 @@
 import SwiftUI
 
 struct TopPartDetailView: View {
-    // MARK: - PROPERTIES
-    
-    @State private var isAnimating: Bool = false
-    
-    // MARK: - BODY
     var body: some View {
-        HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 6, content: {
-            // PRICE
-            VStack(alignment: .leading, spacing: 6, content: {
-                Text("Price")
-                    .fontWeight(.semibold)
-                
-                Text(sampleProduct.formattedPrice)
-                    .font(.largeTitle)
-                    .fontWeight(.black)
-                    .scaleEffect(1.35, anchor: .leading)
-            })
-            .offset(y: isAnimating ? -50 : -75)
-            
-            Spacer()
-            
-            // PHOTO
-            Image(sampleProduct.image)
-                .resizable()
-                .scaledToFit()
-                .offset(y: isAnimating ? 0 : -35)
-        }) //: HSTACK
-        onAppear(perform: {
-            withAnimation(.easeOut(duration: 0.75)) {
-                isAnimating.toggle()
-            }
-        })
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
-// MARK: - PREVIEW
-#Preview(traits: .sizeThatFitsLayout) {
+#Preview {
     TopPartDetailView()
-        .padding()
 }
