ca14348b9b1ab727e2394cc83ee66ec400149e1e
sbotchey23 <sbotchey23@hotmail.com>
2024-02-11 07:53:19 +0000

    updated: TopPartDetailView

--- SWIFTUI-APP-TOUCHDOWN/Students/Touchdown/Touchdown/View/Detail/TopPartDetailView.swift
+++ SWIFTUI-APP-TOUCHDOWN/Students/Touchdown/Touchdown/View/Detail/TopPartDetailView.swift
@@ -8,11 +8,43 @@
 import SwiftUI
 
 struct TopPartDetailView: View {
+    // MARK: - PROPERTIES
+    @State private var isAnimating: Bool = false
+    
+    // MARK: - BODY
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(alignment: .center, spacing: 6, content: {
+            // PRICE
+            VStack(alignment: .leading, spacing: 6, content: {
+                Text("Price")
+                    .fontWeight(.semibold)
+                
+                Text(sampleProduct.formattedPrice)
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                    .scaleEffect(1.35, anchor: .leading)
+            })
+            .offset(y: isAnimating ? -50 : -75)
+            
+            Spacer()
+            
+            // PHOTO
+            Image(sampleProduct.image)
+                .resizable()
+                .scaledToFit()
+                .offset(y: isAnimating ? 0 : -35)
+        }) //: HSTACK
+        .padding(5)
+        .onAppear(perform: {
+            withAnimation(.easeOut(duration: 0.75)) {
+                isAnimating.toggle()
+            }
+        })
     }
 }
 
-#Preview {
+// MARK: - PREVIEW
+#Preview(traits: .sizeThatFitsLayout) {
     TopPartDetailView()
+        .padding()
 }
