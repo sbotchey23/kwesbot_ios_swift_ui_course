b676d44e52b341fdfabc23304397de092dca875d
sbotchey23 <sbotchey23@hotmail.com>
2024-02-23 07:18:27 +0000

    added: Constant. updated: Persistence, ContentView, xcdatamodel

---
+++ DEVOTE/Students/Devote/Devote/Utility/Constant.swift
@@ -0,0 +1,21 @@
+//
+//  Constant.swift
+//  Devote
+//
+//  Created by Stephen Kwesi Botchey on 23/02/2024.
+//
+
+import SwiftUI
+
+// MARK: - FORMATTER
+let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+// MARK: - UI
+
+// MARK: - UX
+
