0e7a9973bad51a58b4e8f9920f43a911e6604b1d
sbotchey23 <sbotchey23@hotmail.com>
2024-03-27 03:12:59 +0000

    add notes function added to ContentView
    Added function and updated UI with text field and add button with custom styling

--- NOTES-APP/Students/Notes/Notes Watch App/View/ContentView.swift
+++ NOTES-APP/Students/Notes/Notes Watch App/View/ContentView.swift
@@ -8,17 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: - PROPERTY
+    @State private var notes: [Note] = [Note]()
+    @State private var text: String = ""
+    
+    // MARK: - FUNCTION
+    func save() {
+        dump(notes)
+    }
+    
+    // MARK: - BODY
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "note.text")
-                .imageScale(.large)
-                .foregroundStyle(.accent)
-            Text("Add note")
-        }
-        .padding()
+        NavigationStack {
+            VStack {
+                HStack(alignment: .center, spacing: 6) {
+                    TextField("Add New Note", text: $text)
+                    
+                    Button(action: {
+                        // 1. only run the button action if the text field is not empty
+                        guard text.isEmpty == false else { return }
+                        
+                        // 2. create a new note item then initialize it with the text field value
+                        let note = Note(id: UUID(), text: text)
+                        
+                        // 3. add new note item to the notes array with (append)
+                        notes.append(note)
+                        
+                        // 4. make text field empty
+                        text = ""
+                        
+                        // 5. Save the notes (function)
+                        save()
+                        
+                        
+                    }, label: {
+                        Image(systemName: "plus.circle")
+                            .font(.system(size: 42, weight: .semibold))
+                    })
+                    .fixedSize()
+                    .buttonStyle(PlainButtonStyle())
+                    .foregroundStyle(.accent)
+                    //.buttonStyle(BorderedButtonStyle(tint: .accentColor))
+                } //: HSTACK
+                Spacer()
+                
+                Text("No. of notes = \(notes.count)")
+            } //: VSTACK
+            .navigationTitle("Notes")
+        } //: NAVIGATIONSTACK
     }
 }
 
+// MARK: - PREVIEW
 #Preview {
     ContentView()
 }
