591b642f33b71cd6ce9cb257201a34be9ac25ad7
sbotchey23 <sbotchey23@hotmail.com>
2024-05-29 07:31:15 +0100

    Added: AppView

---
+++ SWIFTUI-APP-02/Students/Avocados/Avocados/View/AppView.swift
@@ -0,0 +1,18 @@
+//
+//  AppView.swift
+//  Avocados
+//
+//  Created by Kwesi A Botchey on 29/05/2024.
+//
+
+import SwiftUI
+
+struct AppView: View {
+    var body: some View {
+        Text("App View")
+    }
+}
+
+#Preview {
+    AppView()
+}
