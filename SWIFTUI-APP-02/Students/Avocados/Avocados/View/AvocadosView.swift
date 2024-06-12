dc9875caf75399c4e48fc8a7575b6c91910cd3c8
sbotchey23 <sbotchey23@hotmail.com>
2024-06-12 08:39:08 +0100

    updated Avocados view

--- SWIFTUI-APP-02/Students/Avocados/Avocados/View/AvocadosView.swift
+++ SWIFTUI-APP-02/Students/Avocados/Avocados/View/AvocadosView.swift
@@ -16,9 +16,20 @@ struct AvocadosView: View {
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 240, height: 240)
-            Text("Avocados")
+                .shadow(color: Color("ColorBlackTransparentDark"), radius: 12, x: 0, y: 8)
+            
+            VStack {
+                Text("Avocados")
+                    .font(.system(size: 42, weight: .bold, design: .serif))
+                    .foregroundStyle(Color.white)
+                    .padding()
+                .shadow(color: Color("ColorBlackTransparentDark"), radius: 4, x: 0, y: 4)
+                
+                Text("Creamy, green, and full of nutrients! Avocado is a powerhouse ingredient at any meal. Enjoy these handpicked avocado recipes for breakfast, lunch, dinner & more!")
+            } //: VSTACK
             
             Spacer()
+            
         } //: VSTACK
         .background(
             Image("background")
