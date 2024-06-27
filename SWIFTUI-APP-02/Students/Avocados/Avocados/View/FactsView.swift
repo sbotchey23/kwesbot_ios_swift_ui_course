15ad837c132adcb53a3302c1c4880b7aa4e02164
sbotchey23 <sbotchey23@hotmail.com>
2024-06-27 15:05:17 +0200

    added: FactsView

--- SWIFTUI-APP-02/Students/Avocados/Avocados/View/FactsView.swift
+++ SWIFTUI-APP-02/Students/Avocados/Avocados/View/FactsView.swift
@@ -10,6 +10,14 @@ import SwiftUI
 struct FactsView: View {
     var body: some View {
         Text("It’s little wonder there are so many health benefits when a single serve of avocado (1/4 or 50g) boasts healthy fats, fibre, folate, niacin, vitamins C, B5, E & K, potassium and antioxidants.")
+            .padding()
+            .frame(width: 300, height: 135, alignment: .center)
+            .background(LinearGradient(gradient: Gradient(colors: [Color("ColorGreenMedium"), Color("ColorGreenLight")]), startPoint: .leading, endPoint: .trailing))
+            .clipShape(RoundedRectangle(cornerRadius: 12))
+            .lineLimit(6)
+            .multilineTextAlignment(.leading)
+            .font(.footnote)
+            .foregroundStyle(Color.white)
     }
 }
 
