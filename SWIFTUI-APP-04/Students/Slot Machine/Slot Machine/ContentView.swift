e7e027b087c9d4152c6ae214bdcff7718194c061
sbotchey23 <sbotchey23@hotmail.com>
2024-08-14 09:16:11 +0100

    new project start slot machine app
    added new project
    added some assets

---
+++ SWIFTUI-APP-04/Students/Slot Machine/Slot Machine/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Slot Machine
+//
+//  Created by Kwesi A Botchey on 30/07/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
