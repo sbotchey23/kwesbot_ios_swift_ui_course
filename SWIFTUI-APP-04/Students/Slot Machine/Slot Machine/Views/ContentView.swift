e6c2f2bddb69d720b63835d97014f6eb4a468282
sbotchey23 <sbotchey23@hotmail.com>
2024-09-04 08:31:03 +0100

    added launch icon and updated launch screen plist

--- SWIFTUI-APP-04/Students/Slot Machine/Slot Machine/Views/ContentView.swift
+++ SWIFTUI-APP-04/Students/Slot Machine/Slot Machine/Views/ContentView.swift
@@ -8,17 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
+        ZStack {
             Text("Hello, world!")
-        }
+        } //: ZSTACK
         .padding()
     }
 }
 
+// MARK: - PREVIEW
 #Preview {
     ContentView()
 }
