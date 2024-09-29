605b2a1c80ef8c370e4f3a487c687cb1ab1f921b
sbotchey23 <sbotchey23@hotmail.com>
2024-09-29 08:01:52 +0100

    added animations and sounds to ContentView
    added PlaySound file

--- SWIFTUI-APP-04/Students/Slot Machine/Slot Machine/Views/ContentView.swift
+++ SWIFTUI-APP-04/Students/Slot Machine/Slot Machine/Views/ContentView.swift
@@ -11,14 +11,17 @@ struct ContentView: View {
     // MARK: - PROPERTIES
     let symbols = ["gfx-bell", "gfx-cherry", "gfx-coin", "gfx-grape", "gfx-seven", "gfx-strawberry"]
     
-    @State private var highscore: Int = 0
+    @State private var highscore: Int = UserDefaults.standard.integer(forKey: "HighScore")
     @State private var coins: Int = 100
     @State private var betAmount: Int = 10
     @State private var reels: Array = [0, 1, 2]
     @State private var showingInfoView: Bool = false
     @State private var isActiveBet10: Bool = true
     @State private var isActiveBet20: Bool = false
-    @State private var showingModal: Bool = true
+    @State private var showingModal: Bool = false
+    @State private var animatingSymbol: Bool = false
+    @State private var animatingModal: Bool = false
+
     
     // MARK: - FUNCTIONS
     
@@ -30,6 +33,7 @@ struct ContentView: View {
         reels = reels.map({ _ in
             Int.random(in: 0...symbols.count - 1)
         })
+        playSound(sound: "spin", type: "mp3")
     }
     
     // CHECK THE WINNING
@@ -41,6 +45,8 @@ struct ContentView: View {
             // NEW HIGH SCORE
             if coins > highscore {
                 newHighScore()
+            } else {
+                playSound(sound: "win", type: "mp3")
             }
         } else {
             // PLAYER LOSES
@@ -54,6 +60,8 @@ struct ContentView: View {
     
     func newHighScore() {
         highscore = coins
+        UserDefaults.standard.set(highscore, forKey: "HighScore")
+        playSound(sound: "high-score", type: "mp3")
     }
     
     func playerLoses() {
@@ -64,12 +72,14 @@ struct ContentView: View {
         betAmount = 20
         isActiveBet20 = true
         isActiveBet10 = false
+        playSound(sound: "casino-chips", type: "mp3")
     }
     
     func activateBet10() {
         betAmount = 10
         isActiveBet10 = true
         isActiveBet20 = false
+        playSound(sound: "casino-chips", type: "mp3")
     }
     
     // GAME IS OVER
@@ -77,7 +87,15 @@ struct ContentView: View {
         if coins <= 0 {
             //SHOW MODAL WINDOW
             showingModal = true
+            playSound(sound: "game-over", type: "mp3")
         }
+    }
+    
+    func resetGame() {
+        UserDefaults.standard.set(0, forKey: "HighScore")
+        highscore = 0
+        coins = 100
+        activateBet10()
     }
     
     
@@ -130,7 +148,7 @@ struct ContentView: View {
                 
                 // MARK: - SLOT MACHINE
                 VStack(alignment: .center, spacing: 0){
-                    
+                
                     // MARK: - REEL #1
                     ZStack {
                         ReelView()
@@ -138,9 +156,16 @@ struct ContentView: View {
                         Image(symbols[reels[0]])
                             .resizable()
                             .modifier(ImageModifier())
+                            .opacity(animatingSymbol ? 1: 0)
+                            .offset(y: animatingSymbol ? 0 : -60)
+                            .animation(.easeOut(duration: Double.random(in: 0.8...1.4)), value: animatingSymbol)
+                            .onAppear(perform: {
+                                self.animatingSymbol.toggle()
+                            })
                     } //: ZSTACK
                     
                     HStack(alignment: .center, spacing: 0) {
+                        
                         // MARK: - REEL #2
                         ZStack {
                             ReelView()
@@ -148,6 +173,12 @@ struct ContentView: View {
                             Image(symbols[reels[1]])
                                 .resizable()
                                 .modifier(ImageModifier())
+                                .opacity(animatingSymbol ? 1: 0)
+                                .offset(y: animatingSymbol ? 0 : -60)
+                                .animation(.easeOut(duration: Double.random(in: 0.8...1.4)), value: animatingSymbol)
+                                .onAppear(perform: {
+                                    self.animatingSymbol.toggle()
+                                })
                         } //: ZSTACK
                         
                         Spacer()
@@ -159,6 +190,12 @@ struct ContentView: View {
                             Image(symbols[reels[2]])
                                 .resizable()
                                 .modifier(ImageModifier())
+                                .opacity(animatingSymbol ? 1: 0)
+                                .offset(y: animatingSymbol ? 0 : -60)
+                                .animation(.easeOut(duration: Double.random(in: 0.8...1.4)), value: animatingSymbol)
+                                .onAppear(perform: {
+                                    self.animatingSymbol.toggle()
+                                })
                         } //: ZSTACK
                     } //: HSTACK
         
@@ -166,13 +203,24 @@ struct ContentView: View {
                     
                     // MARK: - SPIN BUTTON
                     Button(action: {
-                        // SPIN THE REELS
+                        // 1. SET DEFAULT STATE: NO ANIMATION
+                        withAnimation {
+                            self.animatingSymbol = false
+                        }
+
+                        // 2. SPIN THE REELS WITH CHANGING THE SYMBOLS
                         self.spinReels()
                         
-                        // CHECK WINNING
+                        // 3. TRIGGER THE ANIMATION AFTER THE SYMBOLS CHANGE
+                        withAnimation {
+                            self.animatingSymbol = true
+                            
+                        }
+                        
+                        // 4. CHECK WINNING
                         self.checkWinning()
                         
-                        // CHECK GAME IS OVER
+                        // 5. CHECK GAME IS OVER
                         self.isGameOver()
                         
                     }) {
@@ -203,15 +251,19 @@ struct ContentView: View {
                         
                         Image("gfx-casino-chips")
                             .resizable()
+                            .offset(x: isActiveBet20 ? 0 : 20)
                             .opacity(isActiveBet20 ? 1 : 0)
                             .modifier(CasinoChipsModifier())
                         
                     } //: HSATCK
                     
+                     Spacer()
+                    
                     // MARK: - BET 10
                     HStack(alignment: .center, spacing: 10) {
                         Image("gfx-casino-chips")
                             .resizable()
+                            .offset(x: isActiveBet10 ? 0 : -20)
                             .opacity(isActiveBet10 ? 1 : 0)
                             .modifier(CasinoChipsModifier())
                         
@@ -233,11 +285,11 @@ struct ContentView: View {
             } //: VSTACK
             
             
-            // MARK: - BUTTONS
+            // MARK: - OVERLAY BUTTONS
             .overlay(
                 // RESET
                 Button(action: {
-                    print("Reset the game!")
+                    self.resetGame()
                 }, label: {
                     Image(systemName: "arrow.triangle.2.circlepath.circle")
                     })
@@ -262,9 +314,73 @@ struct ContentView: View {
             // MARK: - POPUP
             if $showingModal.wrappedValue {
                 ZStack {
-                    Color(UIColor(red: 0, green: 0, blue: 0, alpha: 0.7))
+                    Color("ColorTransparentBlack")
                         .ignoresSafeArea(.all)
-                }
+                    
+                    // MODAL
+                    VStack(spacing: 0) {
+                        // TITLE
+                        Text("GAME OVER")
+                            .font(.system(.title, design: .rounded))
+                            .fontWeight(.heavy)
+                            .padding()
+                            .frame(minWidth: 0, maxWidth: .infinity)
+                            .background(Color("ColorPink"))
+                            .foregroundStyle(Color.white)
+                        
+                        Spacer()
+                        
+                        // MESSAGE
+                        VStack(alignment: .center, spacing: 16) {
+                            Image("gfx-seven-reel")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(maxHeight: 72)
+                            
+                            Text("Bad luck! You lost all of your coins. \nLets play again!")
+                                .font(.system(.body, design: .rounded))
+                                .lineLimit(2)
+                                .multilineTextAlignment(.center)
+                                .foregroundStyle(Color.gray)
+                                .layoutPriority(1)
+                            
+                            Button(action: {
+                                self.showingModal = false
+                                self.animatingModal = false
+                                self.activateBet10()
+                                self.coins = 100
+                            }) {
+                                Text("New Game".uppercased())
+                                    .font(.system(.body, design: .rounded))
+                                    .fontWeight(.semibold)
+                                    .tint(Color("ColorPink"))
+                                    .padding(.horizontal, 12)
+                                    .padding(.vertical, 8)
+                                    .frame(minWidth: 128)
+                                    .background(
+                                        Capsule()
+                                            .strokeBorder(lineWidth: 1.75)
+                                            .foregroundStyle(Color("ColorPink"))
+                                    )
+                            }
+                            
+                        } // VSTACK
+                        
+                        Spacer()
+                        
+                    } // VSTACK
+                    .frame(minWidth: 280, idealWidth: 280, maxWidth: 320, minHeight: 260, idealHeight: 280, maxHeight:  320, alignment: .center)
+                    .background(Color.white)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                    .shadow(color: Color("ColorTransparentBlack"), radius: 6, x: 0, y: 8)
+                    .opacity($animatingModal.wrappedValue ? 1 : 0)
+                    .offset(y: $animatingModal.wrappedValue ? 0 : -100)
+                    .animation(Animation.spring(response: 0.6, dampingFraction: 1.0, blendDuration: 1.0), value: animatingModal)
+                    .onAppear(perform: {
+                        self.animatingModal = true
+                    })
+                    
+                } //: ZSTACK
             }
             
         } //: ZSTACK
