664aaa2ca4938e6787f7ec57caaa3cd9c8faab3d
sbotchey23 <sbotchey23@hotmail.com>
2024-10-21 18:28:00 +0100

    added: Project starter files

---
+++ SWIFTUI-APP-03/Students/Honeymoon/Honeymoon/HoneymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonApp.swift
+//  Honeymoon
+//
+//  Created by Kwesi A Botchey on 21/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
